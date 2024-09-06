7c6b2eeb22492967abfcf1b3e04ec5dde19f053d
Sergiornr <sergioren45@gmail.com>
2024-09-06 12:05:24 -0300

    CLASE_XII

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,4 @@
-import entidades.Producto;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Scanner;
+import entidades.Cuenta;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,11 +6,14 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Producto ollaAcero = new Producto("Eccen", 50000.0, 70000.0, 20 );
-        ollaAcero.stockDisponible();
-        System.out.println(ollaAcero.stockDisponible());
-        ollaAcero.calcularGanancia();
-        System.out.println(" La ganancia es de : " + ollaAcero.calcularGanancia());
+        Cuenta sergio = new Cuenta("Juan Perez", 54078976,200000.0,34455566,"JuanPerez12");
+
+        System.out.println(sergio.toString());
+        sergio.retirarCuenta(300000);
+        sergio.transferirCuenta( 30000);
+       // System.out.println(ollaAcero.stockDisponible());
+        //ollaAcero.calcularGanancia();
+       // System.out.println(" La ganancia es de : " + ollaAcero.calcularGanancia());
 
 
 
---
+++ src/entidades/Cuenta.java
@@ -0,0 +1,49 @@
+package entidades;
+
+public class Cuenta {
+    //Pablo es cliente de un banco X,
+    // cuando abrió su caja de ahorro brindó su nombre, dni,
+    // saldo inicial y le asignaron un número de cuenta.
+    // A través de la web el banco X  le permite retirar,
+    // depositar y recibir transferencias a su CBU pero para eso,
+    // precisa que le indiquen su saldo, el titular, su CBU y alias.
+    //
+    //Realizar el UML correspondiente para utilizar la clase caja de ahorros en un programa del banco.
+    //Crear una nueva rama con el n° de clase, guardar en la carpeta src el diagrama realizado y
+    // escribir el código correspondiente.
+    private String titular;
+    private Integer dni;
+    private Double saldoInicial;
+    private Integer cbu;
+    private String alias;
+
+    public void retirarCuenta(Double monto){
+
+
+        if (saldoInicial >= monto){
+            saldoInicial = this.saldoInicial- monto;
+            System.out.println(saldoInicial);
+        }else {
+            System.out.println("Fondos Insuficientes");
+        }
+    }
+    public void transferirCuenta( Double monto){
+if (monto <= saldoInicial){
+    transferirCuenta()= saldoInicial - monto;
+    System.out.println(transferirCuenta());
+}
+    }
+    public void depositarCuenta(double deposito){
+
+    }
+    public Cuenta(String titularC,Integer dniC,Double saldoInicialC,Integer cbuC,String aliasC){
+        this.titular = titularC;
+        this.dni = dniC;
+        this.saldoInicial= saldoInicialC;
+        this.cbu = cbuC;
+        this.alias = aliasC;
+    }
+
+
+
+}
