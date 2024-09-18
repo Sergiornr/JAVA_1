2134fe485cc78f3c10f4e33471cdce7bac78e111
Sergiornr <sergioren45@gmail.com>
2024-09-18 20:27:23 -0300

    CLASE_XV

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,31 @@ import java.util.Locale;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args{
+    public static void main(String[] args) {
+
+    }
         //sube Necesitamos crear una  clase Sube que tenga los siguientes atributos: codigo,
         // saldoDisponible,limiteSaldoNegativo. El limite de saldo es común a todas las instancias por lo que
         // corresponde que sea estatico y solo pueda ser modificado por la clase.
         // Armar el constructor y todos los getters y setters
+        public static void main(String[] args) {
+
+            Sube sube1= new Sube(123,2000.0 );
+            System.out.println("ver el límite del saldo negativo");
+            System.out.println( "desde la clase "+ Sube.getLimiteSaldoNegativo2()
+                    + "\n"+ "desde el objeto "+ sube1.getLimiteSaldoNegativo()
+            );
+
+            Sube.setLimiteSaldoNegativo(5000.0);
+            System.out.println(Sube.getLimiteSaldoNegativo2());
 
 
 
+
+
+
+
+        }
 
 
 
@@ -22,7 +39,13 @@ public class Main {
 
 
 
-}
+
+
+
+
+
+
+
 
 
 
--- src/Sube.java
+++ src/Sube.java
@@ -4,7 +4,35 @@ public class Sube {
     private Double saldoDisponible;
     private Double limiteSaldoNegativo;
 
+    public Sube(int codigo, double saldoDisponible) {
+        this.codigo = codigo;
+        this.saldoDisponible = saldoDisponible;
+    }
+
+    public static void setLimiteSaldoNegativo(double nuevoLimite) {
+        limiteSaldoNegativo = nuevoLimite;
+
+    }
+
+    // acceder desde el objeto
+    public double getLimiteSaldoNegativo() {
+        return limiteSaldoNegativo;
+    }
+
+    // acceder desde la clase
+    public static double getLimiteSaldoNegativo2() {
+        return limiteSaldoNegativo;
+    }
 
 
+    public double getSaldoDisponible() {
+        return saldoDisponible;
+    }
+
+    public void setSaldoDisponible(double saldoDisponible) {
+        this.saldoDisponible = saldoDisponible;
+    }
 
 }
+}
+
