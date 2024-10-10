84f56e1455bd558b4bd60880b6978ee98d78e46e
sergiornr <sergioren45@gmail.com>
2024-10-10 12:05:42 -0300

    CLASE_XXV

---
+++ src/CajaAhorro.java
@@ -0,0 +1,17 @@
+public class CajaAhorro extends Cuenta {
+    private Double tasaInteres;
+
+
+    public CajaAhorro(Double saldo, Cliente cliente, Double tasaInteres) {
+        super(saldo, cliente);
+        this.tasaInteres = tasaInteres;}
+        @Override
+        public void depositarEfectivo(Double deposito){
+
+        }
+        @Override
+        public Double extraerEfectivo(Double monto){
+            return 0.0;
+        }
+
+}
---
+++ src/Cuenta.java
@@ -0,0 +1,22 @@
+public abstract class  Cuenta {
+
+    private Double saldo;
+    private Cliente cliente;
+
+    public Cuenta(Double saldo, Cliente cliente) {
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public abstract void depositarEfectivo(Double deposito);
+
+
+    public abstract Double extraerEfectivo(Double monto);
+    public void informarSaldo(){
+        System.out.println("tiene saldo disponible "+ saldo);
+    }
+}
