0342164da6b4b69df6355a67a8abf683ab1763d0
sergiornr <sergioren45@gmail.com>
2024-10-17 12:06:19 -0300

    CLASE_XXV

--- src/CuentaCorriente.java
+++ src/CuentaCorriente.java
@@ -1,12 +1,50 @@
-//public class CuentaCorriente  Cuenta{
-    //private Double montoDescubiertoAutorizado;
+public class CuentaCorriente extends Cuenta {
+    private static Double limiteMontoAutorizado = 50000.0;
+    private Double montoDescubiertoAutorizado;
 
-    //public CuentaCorriente(Double saldo, Cliente cliente, Double montoDescubiertoAutorizado) {
-        //super(saldo, cliente);
-        //this.montoDescubiertoAutorizado = montoDescubiertoAutorizado;
-    //}
+    public CuentaCorriente(Double saldo, Cliente cliente) {
+        super(saldo, cliente);
+        this.montoDescubiertoAutorizado = limiteMontoAutorizado;
+    }
 
-    //public void depositarCheque(Double cheque){
+    public void depositarCheque(Double cheque) {
 
-    //}
-//}
+    }
+
+    @Override
+    public Double extraerEfectivo(Double monto) {
+        if (getSaldo() >= monto) {
+            Double saldoNuevo = getSaldo() - monto;
+            setSaldo(saldoNuevo);
+            System.out.println("Este es el saldo nuevo "+ getSaldo());
+            return monto;
+        } else if (getSaldo()+ montoDescubiertoAutorizado>= monto) {
+            Double descubiertoSolicitado= monto - getSaldo();
+            setSaldo(0.0);
+            montoDescubiertoAutorizado = montoDescubiertoAutorizado - descubiertoSolicitado;
+            System.out.println("Este es el monto descubierto disponible "+ montoDescubiertoAutorizado);
+            return monto;
+        }
+        System.out.println("indique un monto valido ");
+        return null;
+    }
+
+    @Override
+    public void depositarEfectivo(Double deposito) {
+        if (deposito <= 0){
+            System.out.println("Ingrese un monto valido ");
+        } else if (!(montoDescubiertoAutorizado.equals(limiteMontoAutorizado))){
+           Double deuda = limiteMontoAutorizado - montoDescubiertoAutorizado;
+           montoDescubiertoAutorizado = montoDescubiertoAutorizado + deuda;
+           deposito=  deposito - deuda;
+           setSaldo(getSaldo()+ deposito);
+            System.out.println("El saldo quedo en "+getSaldo()+"el monto disponible es "+montoDescubiertoAutorizado);
+        } else  {
+            setSaldo(getSaldo()+ deposito);
+            System.out.println(" Saldo actual "+ getSaldo());
+        }
+
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,15 @@ public class Main {
 Cliente cliente1 = new Cliente(123789,"Fernandez",45789765,120988765);
 
 CajaAhorro cajaAhorro1 = new CajaAhorro(200000.0,cliente1,24.9);
-//CuentaCorriente cuentaCorriente = new CuentaCorriente(289765.0,cliente1,600000.0);
+CuentaCorriente cuentaCorriente = new CuentaCorriente(3000.0,cliente1);
  cajaAhorro1.depositarEfectivo(30000.0);
  cajaAhorro1.extraerEfectivo(50000.0);
+        System.out.println("metodos de cuenta corriente ");
+ cuentaCorriente.extraerEfectivo(20000.0);
+ cuentaCorriente.depositarEfectivo(30000.0);
+
+
+
 
 
 
