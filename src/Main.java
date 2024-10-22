7b297095ed7bce504702ec5f5744269dd50ba2b9
Sergiornr <sergioren45@gmail.com>
2024-10-22 19:06:31 -0300

    CLASE_XXV

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,23 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
 
-Cliente cliente1 = new Cliente(123789,"Fernandez",45789765,120988765);
+        Cliente cliente1 = new Cliente(123789, "Fernandez", 45789765, 120988765);
 
-CajaAhorro cajaAhorro1 = new CajaAhorro(200000.0,cliente1,24.9);
-CuentaCorriente cuentaCorriente = new CuentaCorriente(3000.0,cliente1);
- cajaAhorro1.depositarEfectivo(30000.0);
- cajaAhorro1.extraerEfectivo(50000.0);
+        CajaAhorro cajaAhorro1 = new CajaAhorro(200000.0, cliente1, 24.9);
+        CuentaCorriente cuentaCorriente = new CuentaCorriente(3000.0, cliente1);
+        CuentaComitente cuentaComitente = new CuentaComitente(15000.0, cliente1, "1234");
+        Cheque cheque1 = new Cheque(40000.0, "Banco Nacion", 12 / 09 / 2024);
+        cajaAhorro1.depositarEfectivo(30000.0);
+        cajaAhorro1.extraerEfectivo(50000.0);
         System.out.println("metodos de cuenta corriente ");
- cuentaCorriente.extraerEfectivo(20000.0);
- cuentaCorriente.depositarEfectivo(30000.0);
-
-
-
-
-
-
+        cuentaCorriente.extraerEfectivo(20000.0);
+        cuentaCorriente.depositarEfectivo(30000.0);
+        // extraer sin clave
+        boolean pudoExtraer = cuentaComitente.extraer(7000);
+        System.out.println("Pudo extraer $7000 sin clave?: " + pudoExtraer);
+        //extraer con clave
+        pudoExtraer = cuentaComitente.extraer(7000.0, "1234");
+        System.out.println("Pudo extraer $7000 con clave?: " + pudoExtraer);
 
 
 
